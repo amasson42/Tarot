f2cc68745a6721430433c531ec48534056c2ccfd
Arthur Masson <amasson@student.42.fr>
2024-12-29 12:02:48 +0100

    Generic SwiftUI features

--- Tarot.swiftpm/Extensions/SwiftUIExtensions.swift
+++ Tarot.swiftpm/Extensions/SwiftUIExtensions.swift
@@ -1,17 +1,60 @@
 import SwiftUI
 
-extension Binding where Value == Bool {
+extension View {
     
-    init<T>(optional: Binding<T?>) {
-        self.init {
-            optional.wrappedValue != nil
-        } set: { value in
-            if value {
-                
-            } else {
-                optional.wrappedValue = nil
+    @ViewBuilder
+    func `if`(_ condition: @autoclosure () -> Bool, transform: (Self) -> some View) -> some View {
+        if condition() {
+            transform(self)
+        } else {
+            self
+        }
+    }
+    
+    @ViewBuilder
+    func onFirstAppear(perform: (() -> Void)? = nil) -> some View {
+        self
+            .modifier(OnFirstAppear(perform: perform))
+    }
+}
+
+struct OnFirstAppear: ViewModifier {
+    
+    let perform: (() -> Void)?
+    @State private var firstTimeCalled = false
+    
+    func body(content: Content) -> some View {
+        content
+            .onAppear {
+                if !firstTimeCalled {
+                    perform?()
+                    firstTimeCalled = true
+                }
+            }
+    }
+}
+
+fileprivate struct Example: View {
+    @State private var hidden: Bool = true
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+                .onAppear {
+                    print("Appearing")
+                }
+                .onFirstAppear { 
+                    print("First time !")
+                }
+                .if (hidden) { v in
+                    v.hidden()
+                }
+            Toggle(isOn: $hidden) {
+                Text("Hide")
             }
         }
-
     }
+}
+
+#Preview {
+    Example()
 }
